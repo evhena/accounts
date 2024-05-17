65f16df7bdd6ad2eb96810f52c03efc603ef3e94
Yevheniia <zhenya.bohdanova@scrumlaunch.com>
2024-05-17 14:40:44 +0200

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Accounts Application</h1>
+      <div className="card">Hello on Profile page!!</div>
+    </div>
+  );
+}
+
+export default App;
